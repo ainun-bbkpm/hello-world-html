5fa6c4c41d632a71e0b6570867a324c6f830baaf
Noval Zorin <noval7sihabibi@gmail.com>
2020-05-11 13:36:42 +0700

    Nh3

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
 
     <footer>
-        by Noval Habibi @2020
+        by Noval Habibi @2020 nh3
     </footer>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
